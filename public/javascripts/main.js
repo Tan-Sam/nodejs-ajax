c2e9a97e1a2f6252db88acdd3dc62a7290b02322
sam <Tan-Sam@users.noreply.github.com>
2017-06-07 22:51:28 +0800

    Commit before adding the rest of functions. the get with jQuery, express..

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,75 @@
 console.log('Hi from main.js');
-$(function(){
-  $.ajax({
-    method: 'GET',
-    url: '/api',
-  }).done(function(data){
-    console.log(data);
-  })
+
+
+function addCandyFunction(){
+    // const tname =  $('.form-control[name=name]');
+    // const tprice = $('.form-control[name=price]');
+
+    $.ajax({
+      method: 'POST',
+      url:  '/api',
+      data: {id:6, name: $('.form-control[name=name]').val(), price: $('.form-control[name=price]').val()}
+    }).done(function(data){
+
+      let newRowHtml = $('#productRowTpl').html();
+
+      newRowHtml = newRowHtml.replace(/{{Name}}/, data.name).replace(/{{Price}}/, data.price);
+
+      // console.log(newRowHtml);
+      $('#cart').append(newRowHtml);
+
+
+      // console.log(data);
+      //  do some add row.
+
+    }).always(function(){
+      console.log('always');
+    });
+
+    //  working   ***********
+    // console.log('im in the function');
+    // $.ajax({
+    //   method: 'POST',
+    //   url: '/api',
+    //   data: {'id':'5', 'name':'some name', 'colors':'red'},
+    // }).done(function(data){
+    //   console.log(data);
+    //
+    // }).fail(function(){
+    //   console.log('error');
+    //
+    // }).always(function(){
+    //   console.log('ending');
+    //
+    // });
 }
-);
+
+
+
+$(function(){
+  $('.btn-success').click(addCandyFunction);
+});
+
+
+
+
+
+
+
+
+// $(function(){
+//   $.ajax({
+//     method: 'POST',
+//     url: '/api',
+//     data: {'id':'5', 'name':'some name', 'colors':'red'},
+//   }).done(function(data){
+//     console.log(data);
+//
+//   }).fail(function(){
+//     console.log('error');
+//
+//   }).always(function(){
+//     console.log('ending');
+//
+//   });
+// });
